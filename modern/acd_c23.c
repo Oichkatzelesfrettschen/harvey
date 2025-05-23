6177ec3e18cc90eb2e450150f7a4e372dcee2790
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-05-23 01:56:52 -0700

    Add recursive spinlock utility

--- modern/acd_c23.c
+++ modern/acd_c23.c
@@ -1,5 +1,6 @@
 #include "args.h"
 #include "msf.h"
+#include "spinlock.h"
 #include <stdbool.h>
 #include <stdio.h>
 
@@ -7,9 +8,19 @@ int main(int argc, char **argv) {
     CmdArgs args = parse_args(argc, argv);
     printf("C23 acd skeleton running on device %s. Verbose=%d\n", args.device, args.verbose);
 
+    /* Example use of the recursive spinlock. */
+    Spinlock lock;
+    spinlock_init(&lock);
+    spinlock_acquire(&lock);
+    /* Re-acquire recursively. */
+    spinlock_acquire(&lock);
+
     /* TODO: implement SCSI access and event loop using pthreads. */
     Msf start = msf_from_frames(0);
     printf("Start at %02d:%02d.%02d\n", start.m, start.s, start.f);
+
+    spinlock_release(&lock);
+    spinlock_release(&lock);
 
     return 0;
 }
